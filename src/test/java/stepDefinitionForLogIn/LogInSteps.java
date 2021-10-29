e2468890ca402247d6699f60bbca513385fdcb50
PMonaheng <palesa>
2021-10-29 13:14:14 +0200

    Cucumber commit

---
+++ src/test/java/stepDefinitionForLogIn/LogInSteps.java
@@ -0,0 +1,29 @@
+package stepDefinitionForLogIn;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LogInSteps {
+    @Given("user is on the login page")
+    public void user_is_on_the_login_page()
+    {
+        System.out.println("Inside step- user is on the login page");
+    }
+    @When("user enters username and password")
+    public void user_enters_username_and_password()
+    {
+        System.out.println("Inside step- user enters username and password");
+    }
+    @And("clicks on login button")
+    public void clicks_login_button()
+    {
+        System.out.println("Inside step- clicks on login button");
+    }
+    @Then("user is navigated to the home page")
+    public void user_is_navigated_to_the_home_page()
+    {
+        System.out.println("Inside step- user is navigated to the home page");
+    }
+}
